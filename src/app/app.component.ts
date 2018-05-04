833e223c6d786d62aa662243e452f2be0b86ef6d
rocky122 <ssdn_prateek@yahoo.com>
2018-05-05 00:53:47 +0530

    working with string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template : `<p> My name is {{name}}</p>`,
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() { }
+
+  // Define all the application variables here pertaining to the component
+  name: string = 'Rocky';
+
+  constructor() { 
+    setTimeout(() => {
+      this.name = 'Arnav'
+    },1000);
+  }
 
   ngOnInit(): void { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
-
+import {ServerComponent} from './server/server.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
